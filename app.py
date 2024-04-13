2774b2c641a994687e5df1fdc32f90aef972033f
Ben Halverson <benhalverson33@gmail.com>
2024-04-13 16:12:52 -0700

    update

--- app.py
+++ app.py
@@ -1,41 +1,60 @@
 import pygame
+from gpiozero import Servo
+from time import sleep
 pygame.init()
+
 pygame.joystick.init()
 
+servo = Servo(11)
+
+
+while True:
+    servo.min()
+    print(servo.value * 10)
+    sleep(1)
+        
+    servo.mid()
+    print(servo.value * 10)
+    sleep(1)
+    servo.max()
+    print(servo.value * 10)
+    sleep(1)
 # Check for joystick presence
-if pygame.joystick.get_count() > 0:
-    # Use the first joystick
-    joystick = pygame.joystick.Joystick(0)
-    joystick.init()
-    print(f"Initialized Joystick : {joystick.get_name()}")
-else:
-    print("No joystick detected!")
-    quit()
+# if pygame.joystick.get_count() > 0:
+#     # Use the first joystick
+#     joystick = pygame.joystick.Joystick(0)
+#     joystick.init()
+#     print(f"Initialized Joystick : {joystick.get_name()}")
+# else:
+#     print("No joystick detected!")
+#     quit()
 
-# Loop to keep the application running
-try:
-    while True:
-        # Update the joystick events
-        pygame.event.pump()
+# # Loop to keep the application running
+# try:
+#     while True:
+#         # Update the joystick events
+#         pygame.event.pump()
+        
+#         left_joystick = joystick.get_axis(0)
+#         print(f"left joystick {left_joystick:.2f}")
 
-        # Iterate through buttons
-        for i in range(joystick.get_numbuttons()):
-            button = joystick.get_button(i)
-            if button:
-                print(f"Button {i} pressed")
+#         print(servo.value)
+#         if left_joystick < 0:
+#             servo.min()
+#         elif left_joystick > 0:
+#             servo.max()
+#         else:
+#             servo.mid()
+#         right_trigger = joystick.get_axis(4)
+#         print(f"right trigger {right_trigger:.2f}")
+ 
+#         left_trigger = joystick.get_axis(5)
+#         print(f"left trigger {left_trigger:.2f}")
 
-        # Axis motion
-        for i in range(joystick.get_numaxes()):
-            axis = joystick.get_axis(i)
-            print(f"Axis {i} value: {axis:.2f}")
+#         a_button = joystick.get_button(0)
+#         print('A button', a_button)
 
-        # Hat switch position
-        for i in range(joystick.get_numhats()):
-            hat = joystick.get_hat(i)
-            print(f"Hat {i} position: {hat}")
+#         pygame.time.wait(100)
 
-        # Small delay to make the output readable
-        pygame.time.wait(100)
-
-except KeyboardInterrupt:
-    print("Application exited.")
+# except KeyboardInterrupt:
+#     print("Application exited.")
